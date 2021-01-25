e3420eed618ae0c9e7b417c1ca425491d055ecb4
carlosnani <carlosnani80@gmail.com>
2021-01-25 06:07:46 -0300

    Add new article form with tinymce

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const router = express.Router();
+
+const Category = require('../categories/Category');
+
 const Article = require('./Article');
+const slugify = require('slugify');
+
  
 router.get('/admin/articles/new', (req, res) => {
-    res.render('./admin/articles/new') 
+    Category.findAll().then((category) => {
+        res.render('./admin/articles/new', {category: category}); 
+    })    
 });
+
+router.post('/articles/save', (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title, { lower: true,}),
+        body: body,
+        categoryid: categoryid
+    });
+})
+
 
  
 module.exports = router;
